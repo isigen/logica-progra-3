2be97a0a1c265ca241e1e1c944ebc52973163ae5
isigen <isidora.abarzua@genstudents.org>
2024-05-17 16:07:35 -0400

    subiendo tare

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Bienvenido a mi programa")
+let nume;
+
+while (true) {  //comprobando que sea un numero
+    nume = parseFloat(prompt("Ingresa tu numero: "))
+    if (!isNaN(nume)){
+        break;
+    }else{
+        alert("Porfavor ingresa un numero valido!");
+    }  
+}
+
+function factorialize(num) {
+    var result = num;
+    if (num === 0 || num === 1) // estos numeros no se pueden factorializar ya que no tienen numeros cercanos al 0
+      return 1; 
+    while (num > 1) { 
+      num--;
+      result *= num;
+    }
+    return result;
+  }
+
+  let resultado = factorialize(nume)
+  console.log("Aqui tienes tu numero factorizado: ", resultado)
